c4624fded49ea7ed0e5a5b72194f058ec989f47f
Henrique Paiva Araujo <henrique.araujo@sipam.gov.br>
2025-08-25 16:15:35 -0400

    projetos Estudo

---
+++ conversor de fc para f.py/conversor.py
@@ -0,0 +1,10 @@
+print("conversor de celsius para fahrenheit")
+print ("digite a temperatura em celsius:")
+celsius = float(input())
+celsius = (celsius * 9/5) + 32
+print("a temperatura em fahrenheit é:", celsius, "*F")
+print ("conversor de Fahrenheit para Celsius")
+print ("digite a temperatura em Fahrenheit:")
+fahrenheit = float(input())
+fahrenheit = (fahrenheit - 32) * 5/9
+print("a temperatura em celsius é:", fahrenheit, "*C")
